61818e99b717093b8325b38d5ade509677497e63
danisltpi <lcdns@protonmail.com>
2025-08-01 11:25:06 +0200

    feat(auth): Create user repository and entity

---
+++ backend/src/main/java/com/stemdrop/backend/auth/model/User.java
@@ -0,0 +1,24 @@
+package com.stemdrop.backend.auth.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role; // "USER", "ADMIN", ...
+
+}
