0eb6ae572e693f95c5810f8dad90f0b50ed86f5f
danisltpi <lcdns@protonmail.com>
2025-08-08 14:21:01 +0200

    feat(auth): Add JWT utility

--- backend/src/main/java/com/stemhub/backend/auth/model/User.java
+++ backend/src/main/java/com/stemhub/backend/auth/model/User.java
@@ -1,23 +1,23 @@
-package com.stemhub.backend.auth.model;
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-@Entity
-@Getter
-@Setter
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    @Column(unique = true, nullable = false)
-    private String username;
-
-    @Column(nullable = false)
-    private String password;
-
-    @Column(nullable = false)
-    private String role; // "USER", "ADMIN", ...
-}
+//package com.stemhub.backend.auth.model;
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Entity
+//@Getter
+//@Setter
+//public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private long id;
+//
+//    @Column(unique = true, nullable = false)
+//    private String username;
+//
+//    @Column(nullable = false)
+//    private String password;
+//
+//    @Column(nullable = false)
+//    private String role; // "USER", "ADMIN", ...
+//}
--- backend/src/main/java/com/stemhub/backend/auth/repository/UserRepository.java
+++ backend/src/main/java/com/stemhub/backend/auth/repository/UserRepository.java
@@ -1,10 +1,10 @@
-package com.stemhub.backend.auth.repository;
-
-import com.stemhub.backend.auth.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
+//package com.stemhub.backend.auth.repository;
+//
+//import com.stemhub.backend.auth.model.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.Optional;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//    Optional<User> findByUsername(String username);
+//}
